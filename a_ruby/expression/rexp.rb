e03e9053b314098ed9c6d40678eb302191d91984
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-06-09 23:21:57 -0700

    Create rexp.rb

--- a_ruby/expression/rexp.rb
+++ a_ruby/expression/rexp.rb
@@ -1,32 +1,3 @@
-##### Regular expression with variable name #####
-var='food'
-s =~ /^#{var}$/
-
-if s =~/^#{var}$/ 
-  
-end
-##### Regular expression with variable name #####
-1. %r{... } : (/... /)
-  expect(rendered).to match %r{<tr><td></td><td></td><td></td><td></td><td></td></tr>}
-
-2. Interpolating a string into a regex
-uid='1234'
-test = ["1234_o365" , "1000"]
-test.find{|x| /#{uid}/ =~ x}
-
-if goo =~ /#{Regexp.quote(uid)}/
-##### Regular expression with variable name #####
-s="link_to 'New Part', new_part_path"
-s.sub(/link_to (.*),(.*)/,'\1\2')
-s.sub(/link_to (.*),(.*)/,'t(\1),\2')
-s.sub(/link_to (.*),(.*)/,'link_to t(\1),\2')
-
-ps={pold:"link_to (.*),(.*)",pnew:"link_to t(\\1),\\2"}
-s.sub(/#{ps[:pold]}/, "#{ps[:pnew]}")
-
-###  .*,  Agressive
-###  .*?, Non-Agressive
-
 #######################################
 ## Add id according to class name
 #file_names = ['foo.txt', 'bar.txt']
