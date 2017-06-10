6f0693ef34947f04b4ac2bece2c3041053e96b79
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-06-09 23:26:56 -0700

    Create capture.rb

--- a_ruby/expression/capture.rb
+++ a_ruby/expression/capture.rb
@@ -1,3 +1,10 @@
+%r{... } : (/... /)
+  expect(rendered).to match %r{<tr><td></td></tr>}
+##### Regular expression with variable name #####
+var='food'
+s =~ /^#{var}$/
+if s =~/^#{var}$/ 
+##### Capture match #####
 string = "RyanOnRails: This is a test"
 string =~ /(^.*)(:)(.*)/
 string.match(/(^.*)(:)(.*)/i)
@@ -7,8 +14,26 @@ if match = string.match(/(^.*)(:)(.*)/i)
 end
 
 if ryan_string =~ /(^.*)(:)(.*)/i
-   first = $1
-   third = $3
+   first, third = $1,$3
 end
 
 one, two, three = Regexp.last_match.captures
+##### Regular expression with variable name #####
+2. Interpolating a string into a regex
+uid='1234'
+test = ["1234_o365" , "1000"]
+test.find{|x| /#{uid}/ =~ x}
+
+if goo =~ /#{Regexp.quote(uid)}/
+  
+##### Regular expression with variable name #####
+s="link_to 'New Part', new_part_path"
+s.sub(/link_to (.*),(.*)/,'\1\2')
+s.sub(/link_to (.*),(.*)/,'t(\1),\2')
+s.sub(/link_to (.*),(.*)/,'link_to t(\1),\2')
+
+ps={pold:"link_to (.*),(.*)",pnew:"link_to t(\\1),\\2"}
+s.sub(/#{ps[:pold]}/, "#{ps[:pnew]}")
+
+###  .*,  Agressive
+###  .*?, Non-Agressive
