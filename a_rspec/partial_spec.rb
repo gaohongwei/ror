8fc93db48c2d1dfe1d0ac1db2a11fd3735f946e0
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-06-15 11:00:26 -0700

    Create partial_spec.rb

--- a_rspec/partial_spec.rb
+++ a_rspec/partial_spec.rb
@@ -1,2 +1,3 @@
 expect(rendered).not_to render_template(partial: 'p1')
 expect(rendered).not_to render_template(partial: 'p2')
+expect(rendered).to render_template(partial: 'p3', locals: {name: 'name })
