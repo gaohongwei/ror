c8ddab03f5e02256597e963600eddbc5686025f2
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-06-23 17:57:50 -0700

    Create pass_form.rb

---
+++ a_rspec/ui/pass_form.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+include SimpleForm::ActionViewExtensions::FormHelper
+
+RSpec.describe 'p1.html.slim', type: :view do
+  include Devise::TestHelpers
+  let(:admin) { FactoryGirl.create(:admin) }
+  before do
+    @data = FactoryGirl.create(:model_factory)
+    simple_form_for(@data) { |f| @form = f }
+    @form.simple_fields_for(:rules) { |f| @form2 = f }
+  end
+  describe 'activity fields' do
+    it 'show div' do
+      render 'partial', f: @form2
+      expect(rendered).to have_tag(:td, text: 'my div')
+    end
+  end
+end
