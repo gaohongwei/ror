2ef92173210a9ebefc37ebfe0c8add2b71755f4d
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-06-28 11:35:41 -0700

    Update list.rb

--- a_rspec/expect_block/list.rb
+++ a_rspec/expect_block/list.rb
@@ -5,3 +5,7 @@
   expect do
 
   end.to change { described_class.count }.by(1)
+
+  expect do
+    get :show, id: @obj.to_param 
+  end.to raise_error(RailsParam::Param::InvalidParameterError,"Error msg")
