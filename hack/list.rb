634d35c1d292cc8d528aa479e88918ceb08ff4ca
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-06-28 17:20:36 -0700

    Create list.rb

---
+++ hack/list.rb
@@ -0,0 +1,8 @@
+MySQL SQL Injection Cheat Sheet
+http://pentestmonkey.net/cheat-sheet/sql-injection/mysql-sql-injection-cheat-sheet
+
+ASCII to Hex
+http://www.asciitohex.com/
+
+XSS Filter Evasion Cheat Sheet
+https://www.owasp.org/index.php/XSS_Filter_Evasion_Cheat_Sheet
