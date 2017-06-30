8d9617f2a6c246f375a2a7754957e487663ac39f
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-06-29 18:17:24 -0700

    Create nested_parameters.rb

---
+++ topic/nested_parameters.rb
@@ -0,0 +1,44 @@
+class Variant < ActiveRecord::Base
+  belongs_to :product
+  # Attributes: name:string, price:float
+end
+class Image < ActiveRecord::Base
+  belongs_to :product
+  # Attributes: url:string, alt:string caption:string
+end
+class Product < ActiveRecord::Base
+  has_many :variants
+  accepts_nested_attributes_for :image
+end
+
+def product_params
+  params.require(:product).permit(
+    :name, :price,
+    image_attributes: [ :id, :url, :alt, :caption ]
+  )
+end
+
+= form_for @product do |f|
+
+  // Product attributes
+  .form-group
+    = f.label :name
+    = f.text_field :name  
+  .form-group
+    = f.label :price
+    = f.text_field :price
+
+  // Image attributes
+  = f.fields_for :image do |f|
+    = f.label :url
+    = f.text_field :url
+
+    = f.label :alt
+    = f.text_field :alt
+
+    = f.label :caption
+    = f.text_field :caption
+
+  = f.submit
+
+
