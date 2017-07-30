78cfa14f2301b13424968eabb11f75b2d7c713f4
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-07-29 21:46:31 -0700

    Update call_chain.rb

--- a_ruby/tools/call_chain.rb
+++ a_ruby/tools/call_chain.rb
@@ -4,3 +4,14 @@ end
 
 arr = [:to_i, [:+, 4], :to_s, [:*, 3]]
 call_chain('1', arr)
+
+def call_chain(obj, arr)
+  out = obj
+  arr.each do |arg|
+    out = out.send(*arg) if out.respond_to?(*arg)
+  end
+  out
+end
+
+arr = [:to_i, [:+, 4], :to_s, [:*, 3], [:what, 10]]
+call_chain('1', arr)
