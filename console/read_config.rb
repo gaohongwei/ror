8830339392d5271f23785d1e149e3c711b8b3f20
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-09-07 12:27:24 -0700

    Update read_config.rb

--- console/read_config.rb
+++ console/read_config.rb
@@ -3,9 +3,18 @@
 include ApplicationHelper
 
 Print a Rails configuration in console
+Rails.application.config
 Rails.application.config.action_mailer
 Rails.application.config.<your_variable>
 Rails.application.config.instance_variables
+
+============= Routes
+Rails.application.routes
+
+In the Rails console, the variable app holds a session object 
+on which you can call path and url helpers as instance methods. 
+app
+app.user_path(User.first)
 
 =============Mongoid Config
 Mongoid::Config.sessions => {"default"=>{"database"=>"aperture_development", "hosts"=>["localhost:27017"], "options"=>nil}}
