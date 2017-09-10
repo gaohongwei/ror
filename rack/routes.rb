99eb95bb420aae2209c2429ae021d25a632b071f
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-09-10 13:38:57 -0700

    Update routes.rb

--- rack/routes.rb
+++ rack/routes.rb
@@ -11,3 +11,18 @@ end
 
 The use method can also be used here to specify middleware to run under a specific path:
 This example includes a piece of middleware which will run before requests hit Heartbeat.
+
+  class HelloWorld
+  def call(env)
+    req = Rack::Request.new(env)
+    case req.path_info
+    when /hello/
+      [200, {"Content-Type" => "text/html"}, ["Hello World!"]]
+    when /goodbye/  
+      [500, {"Content-Type" => "text/html"}, ["Goodbye Cruel World!"]]
+    else
+      [404, {"Content-Type" => "text/html"}, ["I'm Lost!"]]
+    end
+  end
+end
+run HelloWorld.new
