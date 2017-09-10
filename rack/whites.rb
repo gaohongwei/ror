56618e9a530e1150be0b0042da8b9cfbb5a63870
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-09-10 10:31:48 -0700

    Create whites.rb

---
+++ rack/whites.rb
@@ -0,0 +1,30 @@
+##### What is a Rack #####
+A minimal interface between webservers and Ruby frameworks.
+The webservers need to support Ruby
+
+##### How to use Rack #####
+Provide an object that responds to the call method
+  taking the environment hash as a parameter, 
+  returning an array with three elements:
+    [status, headers, body]
+      The HTTP response code
+      A Hash of headers
+      Body
+##### Samples #####
+class JSONServer
+  def call(env)
+    [200, {"Content-Type" => "application/json"}, ['{ "message" : "Hello!" }']]
+  end
+end
+
+JSONServer.new or 
+app = Proc.new do |env|
+    ['200', {'Content-Type' => 'text/html'}, ['A barebones rack app.']]
+end
+
+##### start rack #####
+# config.ru
+run Proc.new { |env| ['200', {'Content-Type' => 'text/html'}, ['get rack\'d']] }
+
+$ rackup config.ru
+
