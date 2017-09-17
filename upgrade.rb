66ce45e2c0493976d4a0818689ff1418f85d955a
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-09-17 09:56:51 -0700

    Create upgrade.rb

---
+++ upgrade.rb
@@ -0,0 +1,19 @@
+From Rails3 to Rails4
+Production NoMethodError: undefined method `sweep' for {"discard"=>[], "flashes"=>{"just_switched"=>true}}:Hash
+Error Message: NoMethodError: undefined method `sweep' for {"discard"=>[], "flashes"=>{"just_switched"=>true}}:Hash
+class Hash
+  def now
+    Rails.logger.warn "Stubbing now during upgrade"
+    {}
+  end
+
+  def keep
+    # stub keep for upgrade purposes
+    Rails.logger.warn "Stubbing keep during upgrade"
+  end
+
+  def sweep
+    # stub sweep for upgrade purposes
+    Rails.logger.warn "Stubbing sweep during upgrade"
+  end
+end
