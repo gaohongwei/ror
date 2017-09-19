bca27f79b55703582c5cbd55ef401bac96cc5d79
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-09-19 16:55:10 -0700

    Create require.rb

---
+++ assets/require.rb
@@ -0,0 +1,5 @@
+require_tree, use relative path to current file
+require, 
+// Order of the files loaded here is important
+//= require javascripts/i18n_stack
+//= require_tree ../frontend/javascripts/webpack
