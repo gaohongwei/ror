b71b96a1e7b97f41ad460a0d0d8550306d79e093
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-09-30 11:48:51 -0700

    Create special_methods.rb

---
+++ a_ruby/methods/special_methods.rb
@@ -0,0 +1,10 @@
+
+instance_variable_get("@#{attr}")
+instance_variable_set("@#{attr}", value)
+
+
+define class method
+singleton_class.
+singleton_class.send :define_method mehod_name do |v|
+
+end
