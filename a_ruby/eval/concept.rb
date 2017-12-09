eb43558f1d71cdf6f5f0477f55ff7b45ba790391
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-12-09 10:24:16 -0800

    Update concept.rb

--- a_ruby/eval/concept.rb
+++ a_ruby/eval/concept.rb
@@ -8,9 +8,23 @@ class A
     self    
   end
 end
-
 obj = A.new
 obj.instance_eval{ print_me }# I am in object
 obj.class_eval{ print_me }   # I am in class
 A.instance_eval{ print_me }  # I am in class
 A.class_eval{ print_me }     # I am in class
+
+class_name = A
+class_name.class_eval{
+  def print_you
+    puts "you are in object"
+  end
+}
+
+class_name.instance_eval{
+  def print_you
+    puts "you are in class"
+  end
+}
+A.new.print_you # you are in object
+A.print_you # you are in class
