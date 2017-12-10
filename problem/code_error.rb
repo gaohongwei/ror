387cc8e147f2900a356b08504ac2fb890c04532a
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2017-12-10 10:34:07 -0800

    Update and rename code_error.rb to code_n_config.rb

--- problem/code_error.rb
+++
@@ -1,3 +0,0 @@
-1. unpermitted parameters
-Array or string have different format
-params.require(:search).permit(:user, users: [])
---
+++ problem/code_n_config.rb
@@ -0,0 +1,7 @@
+1. unpermitted parameters
+Array or string have different format
+params.require(:search).permit(:user, users: [])
+
+2. Cannot render console from 10.0.2.2! 
+Allowed networks: 127.0.0.1, ::1, 127.0.0.0/127.255.255.255
+  config.web_console.whiny_requests = false
