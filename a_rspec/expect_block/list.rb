e31e583d4ba1c4732e939619cda63b87d3da6688
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-01-09 16:02:34 -0800

    Update list.rb

--- a_rspec/expect_block/list.rb
+++ a_rspec/expect_block/list.rb
@@ -9,3 +9,7 @@
   expect do
     get :show, id: @obj.to_param 
   end.to raise_error(RailsParam::Param::InvalidParameterError,"Error msg")
+
+expect{Object.non_existent_message}.to raise_error(NameError)
+expect{Object.non_existent_message}.to raise_error
+expect(ErrorMailer).to receive(:send_email).with(email_subject, email_body)
