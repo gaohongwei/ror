fcd831e9cb60629f26e64afba164277d0dfe082e
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-01-24 15:14:54 -0800

    Update stub.rb

--- a_rspec/stub/stub.rb
+++ a_rspec/stub/stub.rb
@@ -7,4 +7,7 @@
   Service.stub_chain(:service_port).with('authz').and_return('8080')
       
 ## stub_const, Constant
- stub_const("Klass::MAX_PIXEL", 10)
+  stub_const("Klass::MAX_PIXEL", 10)
+  stub_const('ENV', {'RACK_ENV' => 'production'})
+  stub_const("::Session", double(:session, key: 'key'))  
+  ENV.stub(:[]).with("RACK_ENV").and_return("production")
