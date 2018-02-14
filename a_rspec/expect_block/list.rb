7ed28f695ba34960a193be42bc1890f0fcdda5fb
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-02-13 16:04:31 -0800

    Update list.rb

--- a_rspec/expect_block/list.rb
+++ a_rspec/expect_block/list.rb
@@ -13,3 +13,17 @@
 expect{Object.non_existent_message}.to raise_error(NameError)
 expect{Object.non_existent_message}.to raise_error
 expect(ErrorMailer).to receive(:send_email).with(email_subject, email_body)
+
+
+  Expect Exceptions to be raised in rspec
+  expect { object.some_method("Stupid Monkey") }.to raise_error
+  expect { object.some_other_method }.to raise_error "Awful exception"
+  expect { object.method("argparty") }.to raise_error ErrorClass
+
+
+
+  it "should call 'bar' with appropriate arguments" do
+     subject.stub(:bar)
+     subject.foo
+     expect(subject).to have_received(:bar).with("Invalid number of arguments")
+  end
