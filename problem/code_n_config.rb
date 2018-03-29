c95826e2bdfbbd39b1c8e8987813c64ddf80f43a
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-03-29 09:40:34 -0700

    Update code_n_config.rb

--- problem/code_n_config.rb
+++ problem/code_n_config.rb
@@ -5,3 +5,6 @@ params.require(:search).permit(:user, users: [])
 2. Cannot render console from 10.0.2.2! 
 Allowed networks: 127.0.0.1, ::1, 127.0.0.0/127.255.255.255
   config.web_console.whiny_requests = false
+
+3. bootstrap css not loaded
+  Check existing application.css file, should be deleted if you try to use application.scss
