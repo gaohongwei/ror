2f2ce91de8ced6af6c1dd0fd877a362e8ecb295a
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-04-12 11:23:47 -0700

    Create percent_string.rb

---
+++ a_ruby/string/percent_string.rb
@@ -0,0 +1,8 @@
+%i: Array of Symbols
+%q: String
+%r: Regular Expression
+%s: Symbol
+%w: Array of Strings
+%x: Backtick (capture subshell result)
+
+Capital ones such as %I %W can handle Interpolation
