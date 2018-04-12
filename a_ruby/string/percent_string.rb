b87e2a74594d132ae7cebc14b50cac9d1fc96d6c
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-04-12 11:57:16 -0700

    Update percent_string.rb

--- a_ruby/string/percent_string.rb
+++ a_ruby/string/percent_string.rb
@@ -1,8 +1,12 @@
-%i: Array of Symbols
-%q: String
-%r: Regular Expression
-%s: Symbol
-%w: Array of Strings
+%i: Array of Symbols, 
+%s: One symbol, 
 %x: Backtick (capture subshell result)
 
+%q: Quoted string
+%w: Array of Strings, words of string
+%r: Regular Expression, rexp
+
 Capital ones such as %I %W can handle Interpolation
+
+Doc:
+http://ruby-doc.org/core-2.0.0/doc/syntax/literals_rdoc.html
