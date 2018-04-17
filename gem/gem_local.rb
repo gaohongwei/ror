ca628032b0b5211963aace0fdc19ac59d112815f
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-04-17 12:05:38 -0700

    Update gem_local.rb

--- gem/gem_local.rb
+++ gem/gem_local.rb
@@ -1,6 +1,10 @@
+Install gems from local
+##### in default location #####
+  bundler package
+  bundle install --local
 
+  vender/cache
 
-bundler package
-bundle install --local
-
-vender/cache
+##### specific location in gem file #####    
+  gem 'g1', path: 'vendor/gems/abc-0.0.3'
+  gem 'g2', path: 'lib/gems'
