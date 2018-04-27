51358632f1c8ea6d0f62d5acc2cf8e376b0c964d
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-04-27 14:41:11 -0700

    Update version.rb

--- gem/version.rb
+++ gem/version.rb
@@ -4,3 +4,6 @@ So ~> 2.0.0 means ">= 2.0.0 and < 2.1" in version numbers.
 
 so e.g. ~> 2.3 means "equal to 2.3 or greater than 2.3, but less than 3.0", 
 while ~> 2.3.0 would mean "equal to 2.3.0 or greater than 2.3.0, but less than 2.4.0".
+
+
+>= 0.8.5, indicate that any version later than (or equal to) 0.8.5 is acceptable. No upper bound.
