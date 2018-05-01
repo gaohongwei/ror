ed68431d4211a78a8e748a719786d3dd41987e0f
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-04-30 17:21:15 -0700

    Update allow_spec.rb

--- a_rspec/stub/allow_spec.rb
+++ a_rspec/stub/allow_spec.rb
@@ -4,8 +4,13 @@ find  spec -name *.rb|xargs grep allow|sed -e "s/.*allow/allow/"
 allow_any_instance_of(User).to receive(:name).and_return('name')
 allow(Settings).to receive(:exchange) { { enable: true } }
 allow(Settings).to receive(:exchange)and_return({ enable: true })
-allow(controller).to receive(:action_name) { 'show' }
-allow(controller).to receive(:controller_name) { 'global_settings' }
+
+##### controller 
+  allow(controller).to receive(:action_name) { 'show' }
+  allow(controller).to receive(:controller_name) { 'global_settings' }
+##### view 
+  allow(view).to receive(:helper_method)
+
 allow(user).to receive(:full_name).with('xxx').and_return(true)
 
 controller.stub_chain(:remediation_service, :remove_direct_links).and_return(OpenStruct.new(status: 200))
