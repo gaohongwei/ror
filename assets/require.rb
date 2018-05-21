2b6e049db4bfde195cfc11edcba42b88e247ec01
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-05-20 19:51:48 -0700

    Delete require.rb

--- assets/require.rb
+++
@@ -1,5 +0,0 @@
-require_tree, use relative path to current file
-require, 
-// Order of the files loaded here is important
-//= require javascripts/i18n_stack
-//= require_tree ../frontend/javascripts/webpack
