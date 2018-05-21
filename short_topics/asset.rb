bd72c507129723cad1b1a2c8f27362495efad6af
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-05-20 19:51:15 -0700

    Create asset.rb

---
+++ short_topics/asset.rb
@@ -0,0 +1,10 @@
+##### Undefined variable #####
+  SAS synatx error
+    Undefined variable: "$main-color".
+
+js
+  require_tree, use relative path to current file
+  require, 
+  // Order of the files loaded here is important
+  //= require javascripts/i18n_stack
+  //= require_tree ../frontend/javascripts/webpack
