8567d94f1b3a0551bba9e1338c44678037fcc495
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-05-20 19:39:39 -0700

    Update cookies.rb

--- short_topics/cookies.rb
+++ short_topics/cookies.rb
@@ -1,7 +1,17 @@
-Cookies:
-  if request.xhr?
-    saved_data = JSON.parse(cookies[:saved_data])
-  else
-    data_to_save = {}
-    cookies[:saved_data] = {value: data_to_save.to_json}
-  end
+Cookies
+  Cookies are key-value data pairs stored in the user’s browser 
+  until they reach their specified expiration date. 
+
+  With each new request to your server, 
+  browser will send along all the cookies and 
+  you can access them in your controllers and views 
+  like a normal hash. 
+
+  cookies[:name] = { value: "cookies YUM", expires: Time.now + 3600}.
+  cookies[:hair-color] = "blonde"
+  cookies.delete(:hair-color)
+  
+For hash data
+  saved_data = JSON.parse(cookies[:saved_data])
+  data_to_save = {}
+  cookies[:saved_data] = {value: data_to_save.to_json}
