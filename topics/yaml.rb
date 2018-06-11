3554cc31a0a27848d01226fbff0664f048941dce
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-06-11 15:58:49 -0700

    Update yaml.rb

--- topics/yaml.rb
+++ topics/yaml.rb
@@ -1,3 +1,19 @@
+Load yaml file
+  require "yaml"
+
+  test_obj = ["dogs", "cats", "badgers"]
+  yaml_obj = YAML::dump( test_obj )
+  ruby_obj = YAML::load( yaml_obj )         
+  ruby_obj == test_obj # true
+
+  ActiveRecord::Base.configurations = YAML.load_file('config/database.yml')
+  $aws_db_credentials = YAML.load_file('config/database.yml')[Sinatra::Base.environment.to_s].symbolize_keys!
+# Save into yaml file
+  File.open('/tmp/test.yml', 'w') {|f| f.write ActiveRecord::Base.configurations.to_yaml } #Store
+
+  File.open(SETTINGS_FILE, "w") do |file|
+    file.write settings.to_yaml
+  end
 
 Setting.where("var like '%:zh-CN'").update_all(lang_id:1)
 Setting.where("var like '%:en'").update_all(lang_id:2)
@@ -6,7 +22,3 @@ Setting.update_all(section_id:1)
 File.write('a.txt',Setting.all.to_yaml)
 
 File.write('a.txt',Setting.to_json.to_yaml)
-
-File.open(SETTINGS_FILE, "w") do |file|
-  file.write settings.to_yaml
-end
