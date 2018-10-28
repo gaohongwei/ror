1f64d81c8bcd5d27c7e1fedfcf1b2b6a980ba66c
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-10-28 13:44:19 -0700

    Create path.rb

---
+++ routes/path.rb
@@ -0,0 +1,12 @@
+ActionController::Routing::Routes.draw do |map|
+  map.resources :articles, :has_many => :comments, :shallow => true
+  map.resources :comments, :only => [:index]
+  map.root :articles
+end
+
+path
+  article_comments_path(@article) %>
+  new_article_comment_path(@article) %>
+  edit_comment_path(comment) %>
+  <%= link_to "Destroy", comment, :method => :delete, :confirm => "Are you sure?" %>
+  <% form_for [@article, @comment] do |f| %>
