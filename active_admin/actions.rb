9216e4bdf04d36fa7a6f392819c52281917c9e57
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-10-28 11:44:17 -0700

    Update actions.rb

--- active_admin/actions.rb
+++ active_admin/actions.rb
@@ -3,15 +3,17 @@
   config.remove_action_item(:new)
   config.clear_action_items!
 
+  action_item {link_to "Assets" , "/admin/assets" }
+  action_item(only: :index) { link_to "Do something else", some_path }
   action_item(only: :index) {
     link_to "new", new_project_ticket_path(params[:project_id]) if params[:project_id].present?
   }
-  action_item(only: :index) { link_to "Do something else", some_path }
-
   action_item only: [:edit, :show] do
     @post = Post.find(params[:id])
     link_to 'Publish', publish_admin_post_path(post), method: :put
   end
+
+  config.display_action_items :index => :new, :edit => :destroy, :show => [:edit, :destroy, :custom_action]
 
   controller do
     def action_methods
