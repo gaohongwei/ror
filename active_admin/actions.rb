bd3db219928da9e05599ca62f366b662708bdd56
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-10-28 11:35:33 -0700

    Update actions.rb

--- active_admin/actions.rb
+++ active_admin/actions.rb
@@ -6,7 +6,13 @@
   action_item(only: :index) {
     link_to "new", new_project_ticket_path(params[:project_id]) if params[:project_id].present?
   }
-  
+  action_item(only: :index) { link_to "Do something else", some_path }
+
+  action_item only: [:edit, :show] do
+    @post = Post.find(params[:id])
+    link_to 'Publish', publish_admin_post_path(post), method: :put
+  end
+
   controller do
     def action_methods
       params[:project_id].blank? ? super - ['new'] : super
