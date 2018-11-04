a97af0fa8b036cd6f4cbcdbe31f381d2466f1035
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-11-03 20:10:07 -0700

    Update and rename has_many_through.rb to has_many_sample.rb

--- mvc_m/has_many_sample.rb
+++ mvc_m/has_many_sample.rb
@@ -1,21 +1,48 @@
 ======== Explaination and sample ========
 :source define the associated model name for has_many through association
-The :foreign_key option lets you set the name of the foreign key.
-
+:foreign_key option lets you set the name of the foreign key.
+:class_name, used by belongs_to, has_many
+  
 1. has_many through sample,  source/foreign_key
   rgu project    name:string
   rgu data_file  name:string 
   rgu project_file  project_id:integer file_id:integer
 
-  class Project 
-    has_many :project_files
-    has_many :files,  through: :project_files,  source: :data_file
+  class ProjectFile
+    # belongs_to specify a foreign key
+    # foreign key name convention, xxx => xxx_id
+    belongs_to :user    # => a foreign key, user_id
+    belongs_to :project # => a foreign key, project_id
+    belongs_to :data_file, foreign_key: :file_id
+    # Use random name
+    belongs_to :fff, foreign_key: :file_id, class_name: 'DataFile'   
+    # so ProjectFile should include
+    # project_id: integer, file_id: integer, user_id: integer   
   end
 
-  class ProjectFile 
-    belongs_to :project
-    belongs_to :data_file, foreign_key: :file_id
-  end  
+  class Project
+    belongs_to :user
+    #  has_many need 
+    #    class_name: 'ProjectFile'
+    #    foreign_key: :project_id
+    #  mean ProjectFile have a project_id field
+    has_many :project_files
+    has_many :pfiles, class_name: 'ProjectFile', foreign_key: :project_id 
+    # has_many through dont need to specify foreign key
+    #   need
+    #     source: :data_file
+    #   dont need to specify foreign key 
+    #     because foreign key has been specified at through association
+    has_many :data_files, through: :project_files    
+    has_many :dfiles,     through: :project_files, source: :data_file  
+    has_many :files,      through: :project_files, source: :data_file 
+  end
+
+  class DataFile
+    has_many :project_files, foreign_key: :file_id
+    has_many :projects, through: :project_files
+  end
+
 2. has_many through sample,  source
   has_many :owned_groups, class_name: 'Group'
   has_many :joined_groups, -> { where(user_groups: { status: 3 }) },   through: :user_groups, source: :group
