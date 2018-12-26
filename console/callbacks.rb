ffaef04494490025035e4ec7a264268f79e0f6f6
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-12-25 21:35:34 -0800

    Update callbacks.rb

--- console/callbacks.rb
+++ console/callbacks.rb
@@ -3,6 +3,7 @@ _process_action_callbacks
 
 before_actions
   _process_action_callbacks.map { |c| c.filter if c.kind == :before }
+  _process_action_callbacks.map { |c| {c.filter => c.options} if c.kind == :before }
 
 after_actions
   _process_action_callbacks.map { |c| c.filter if c.kind == :after }
