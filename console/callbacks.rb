6069e582278f1cfb5b2fa344c7c3d0dfe86c4ae5
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2018-12-25 21:23:39 -0800

    Create callbacks.rb

---
+++ console/callbacks.rb
@@ -0,0 +1,8 @@
+_process_action_callbacks
+  callback actions are stored here
+
+before_actions
+  _process_action_callbacks.map { |c| c.filter if c.kind == :before }
+
+after_actions
+  _process_action_callbacks.map { |c| c.filter if c.kind == :after }
