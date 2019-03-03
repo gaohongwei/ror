c5178deba92524d4b99be821a28ed341adee888d
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-03-03 10:33:15 -0800

    Update and rename mvc_m/where.rb to mvc_m/query/where_not.rb

--- mvc_m/query/where_not.rb
+++ mvc_m/query/where_not.rb
@@ -1,14 +1,15 @@
-1. MongoDB
+1. AR
+  Artist.where.not(name: 'name1')
+  Foo.includes(:bar).where.not('bars.id' => nil)
+  Foo.includes(:bar).where.not(bars: { id: nil })
+
+  Prefix.where('length(speall) > 2').sort
+
+
+999. MongoDB
   scope :internal, -> { where(external: false) }
   scope :external, -> { where(external: true) }
   scope :valid_people, -> { where(:name.ne=>INVALID_Artist_NAME) }
 
   Artist.where(:name.nin => ['name1', 'name 2'])
   Artist.not_in(name: ['name1', 'name 2'])
-
-2. AR
-  Artist.where.not(name: 'name1')
-  Foo.includes(:bar).where.not('bars.id' => nil)
-  Foo.includes(:bar).where.not(bars: { id: nil })
-
-  Prefix.where('length(speall) > 2').sort
