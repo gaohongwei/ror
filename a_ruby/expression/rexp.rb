82aa1b03a0297244904753d94c98ae22750ca895
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-03-29 14:37:25 -0700

    Update rexp.rb

--- a_ruby/expression/rexp.rb
+++ a_ruby/expression/rexp.rb
@@ -1,3 +1,34 @@
+##################
+Regular Expression Basic
+Anchor
+  ^: line header
+  $: line tailer
+  \A: string header
+  \z: string tailer (no new line)
+  \Z: string tailer
+
+Pattern:
+  /./ - Any character except a newline.
+  /./m - Any character (the m modifier enables multiline mode)
+  /\b/: word boundary
+  /\d/: A digit character ([0-9])
+  /\D/: 
+  /\w/: A word character ([a-zA-Z0-9_])
+  /\W/: non word char, [^a-zA-Z0-9]+
+  /\s/: A whitespace character: /[ \t\r\n\f]/
+  /\S/:
+  /\h/: hex
+  /\H/:
+
+Repetition
+*: >=0
++: >=1
+?: 0/1
+{n}: Exactly n times
+{n,}: n or more times
+{,m}: m or less times
+{n,m}: At least n and at most m times
+
 A regular expression literal is a pattern between slashes or
 between arbitrary delimiters followed by %r
 s =~ /abc/
@@ -69,32 +100,3 @@ module KeywordValidator
       /x).map(&:strip)
   end
 end
-##################
-Regular Expression Basic
-Anchor
-  ^: line header
-  $: line tailer
-  \A: string header
-  \z: string tailer (no new line)
-  \Z: string tailer
-
-Pattern:
-  /./ - Any character except a newline.
-  /./m - Any character (the m modifier enables multiline mode)
-  /\d/: A digit character ([0-9])
-  /\D/: 
-  /\w/: A word character ([a-zA-Z0-9_])
-  /\W/: non word char, [^a-zA-Z0-9]+
-  /\s/: A whitespace character: /[ \t\r\n\f]/
-  /\S/:
-  /\h/: hex
-  /\H/:
-
-Repetition
-*: >=0
-+: >=1
-?: 0/1
-{n}: Exactly n times
-{n,}: n or more times
-{,m}: m or less times
-{n,m}: At least n and at most m times
