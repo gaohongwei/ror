2af7d33ebee3ca8f2b6b2eb299dfbc7c7c4a8403
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-03-31 22:44:02 -0700

    Update token_benefit.rb

--- authentication/jwt/token_benefit.rb
+++ authentication/jwt/token_benefit.rb
@@ -1,10 +1,5 @@
 Benefits of Token-based Authentication
 
-Cross-domain / CORS
-  Cookies and CORS dont mix well across different domains. 
-  A token-based approach allows you to make AJAX calls to any server, on any domain, 
-  because you use an HTTP header to transmit the user information.
-
 Stateless
   Tokens are stateless. There is no need to keep a session store since 
   the token is a self-contained entity that stores all the user information in it.
@@ -18,11 +13,18 @@ Mobile Ready
   Cookies are a problem when it comes to storing user information in native mobile applications. 
   Adopting a token-based approach simplifies this saving process significantly.
 
-CSRF (Cross Site Request Forgery)
-  Because the application does not rely on cookies for authentication, 
-  it is invulnerable to cross-site request attacks.
-
 Performance
-  In terms of server-side load, a network roundtrip (e.g. finding a session on a database) 
-  is likely to take more time than calculating an HMACSHA256 code to validate a token and parsing its contents. 
+  In terms of server-side load, reduce one network roundtrip (e.g. finding a session on a database) 
+  One network query is likely to take more time than calculating an HMACSHA256 code to validate a token and parsing its contents. 
   This makes token-based authentication faster than the traditional alternative.
+
+Cookies
+  Cross-domain / CORS
+    Cookies and CORS dont mix well across different domains. 
+    A token-based approach allows you to make AJAX calls to any server, on any domain, 
+    because you use an HTTP header to transmit the user information.
+
+
+  CSRF (Cross Site Request Forgery)
+    Because the application does not rely on cookies for authentication, 
+    it is invulnerable to cross-site request attacks.
