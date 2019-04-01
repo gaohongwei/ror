99d89a01ff1276d0dcae3cf7e734d12e96c5418a
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-04-01 09:05:12 -0700

    Create scale.rb

---
+++ architect/scale.rb
@@ -0,0 +1,36 @@
+Nginx: 
+  Load balance
+
+Dyno:
+  one bigger vs several small ones
+
+Cache:
+  data:
+    Find repreated used data
+  Page Caching
+  action Caching
+  fragment Caching
+
+Database
+  Read
+    pre-compute table
+      summary table
+    cache
+
+  DB write slow 
+    combine tx
+    batch commit
+    delayed job
+
+  Insert slow
+    add MD5 hash id of uniq data
+    dont use auto id
+    auto id has a limitation of 2b rows
+    int 2,147,483,647, 2b
+
+  database
+    write to master db
+    read from slave
+    run deplayed jon on dedicated instances 
+
+Move to micro-services
