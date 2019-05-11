56ab7d5c62ef3e2f9c3f783a404ecbb1f93e70f1
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-05-11 10:36:43 -0700

    Update errors.rb

--- AA/errors.rb
+++ AA/errors.rb
@@ -1,3 +1,6 @@
 Errors
   undefined method `ml_predicts' for #<MlModel:0x00007fc0bca7a8f8>
     Solutions: check has_many/belongs_to associations in data model
+  checkbox/radio collections are yes/no
+    collection data is nil
+    instance variable is not available in AA form
