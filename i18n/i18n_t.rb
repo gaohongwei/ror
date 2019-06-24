7a8734b65d71f98a6a0ad1886cd5aa3074a866f6
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-06-24 16:49:07 -0700

    Update and rename 18n_t.rb to i18n_t.rb

--- i18n/i18n_t.rb
+++ i18n/i18n_t.rb
@@ -2,9 +2,19 @@
   def i18n_t(key, *args)
     I18n.t(key, :default => key, args)
   end
-  alias_method :tt, :i18n_t
 
 key = 'active_admin.help_index'
 args = {item: 'abc', item1: '123'}
 I18n.t(key, args)
+
+alias_method :tt, :i18n_t
 tt(key, args)
+
+
+def go(x, *args)
+  puts args.class
+  puts args
+  puts *args
+end
+
+go("a", "b", "c")
