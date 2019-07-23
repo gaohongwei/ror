8e98bac63a5ca4e5567504431431eb9c93896117
Kevin(Gaohong) Wei <gaohongwei@gmail.com>
2019-07-22 17:01:56 -0700

    Update version.rb

--- mvc_m/version.rb
+++ mvc_m/version.rb
@@ -1,4 +1,7 @@
-Database Version
+Get Database Version
   rake db:version
   ActiveRecord::Migrator.current_version
   ActiveRecord::Migrator.get_all_versions
+
+Run 
+  rake db:migrate:redo VERSION=20190722123456
